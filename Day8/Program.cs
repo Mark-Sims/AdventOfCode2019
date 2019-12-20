161026ea13c24a3ea739fab167ef34debbdccda6
Mark <marksims@buffalo.edu>
2019-12-19 19:30:18 -0500

    Day 8 part 2 completed

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -55,13 +55,62 @@ namespace Day8
                 multilayeredImage.Add(layer);
             }
 
+            // Part 1
             IEnumerable<int> zerosPerLayer = multilayeredImage.Select(x => x.NumberOf0s);
             int minimumNumberOf0s = zerosPerLayer.Min();
 
             ImageLayer layerNumberWithMinimumZeros = multilayeredImage.Single(x => x.NumberOf0s == minimumNumberOf0s);
-            //IEnumerable<ImageLayer> layerNumberWithMinimumZeros = multilayeredImage.Where(x => x.NumberOf0s == minimumNumberOf0s);
 
             Console.WriteLine(layerNumberWithMinimumZeros.NumberOf1s * layerNumberWithMinimumZeros.NumberOf2s);
+
+            // Part 2
+            ImageLayer finalImage = new ImageLayer();
+
+            // Initialize image with values -1
+            //finalImage.Image = Enumerable.Repeat(Enumerable.Repeat(-1, imageWidth).ToList(), imageHeight).ToList();
+
+
+            for (int i = 0; i < imageHeight; i++)
+            {
+                finalImage.Image.Add(new List<int>());
+                for (int j = 0; j < imageWidth; j++)
+                {
+                    finalImage.Image[i].Add(-1);
+                    for (int l = 0; l < multilayeredImage.Count; l++)
+                    {
+                        var layer = multilayeredImage[l];
+                        if (layer.Image[i][j] == 2)
+                        {
+                            continue;
+                        }
+                        //Console.WriteLine("Setting value at {0}, {1} to {2}. Taken from layer {3}", i, j, layer.Image[i][j], l);
+                        finalImage.Image[i][j] = layer.Image[i][j];
+                        break;
+                    }
+                }
+            }
+
+            PrettyPrintLayer(finalImage);
+        }
+
+        public static void PrettyPrintLayer(ImageLayer layer)
+        {
+            foreach (var row in layer.Image)
+            {
+                string line = "";
+                foreach (var pixel in row)
+                {
+                    if (pixel == 0)
+                    {
+                        line += " ";
+                    }
+                    else
+                    {
+                        line += pixel.ToString();
+                    }
+                }
+                Console.WriteLine(line);
+            }
 
         }
     }
